b3bbb44a129c33fe317a4d4be195ed22a1f44f86
Daniel <dh.312@t-online.de>
2015-03-27 15:52:30 +0100

    Add a "getSize - Methode" in the desk - class to test git

--- src/de/htwg/se/theAmazingLabyrinth/model/desk.java
+++ src/de/htwg/se/theAmazingLabyrinth/model/desk.java
@@ -2,4 +2,11 @@ package de.htwg.se.theAmazingLabyrinth.model;
 
 public class desk {
 
+	//Test------------------------>
+	int size = 1;
+	
+	public int getSize(){
+		return size;
+	}
+	//<----------------------------
 }
