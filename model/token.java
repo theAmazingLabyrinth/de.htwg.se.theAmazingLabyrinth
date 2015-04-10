3d6cb01a1414f9f110eb358044df7bbf2a2ba761
Benjamin Schaefer <beschaef@htwg-konstanz.de>
2015-04-10 08:53:07 +0200

    added variable 'name' and constructor

--- model/token.java
+++ model/token.java
@@ -1,5 +1,12 @@
 package de.htwg.se.theAmazingLabyrinth.model;
 
 public class token {
+	String name = "";
 
+	public token() {
+		name = "standard token";
+	}
+	public token(String s) {
+		name = s;
+	}
 }
