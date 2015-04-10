958c5df57b066dba7cbb37b9420a5af4d4fea042
Benjamin Schaefer <beschaef@htwg-konstanz.de>
2015-04-10 12:13:43 +0200

    added 'ways'

--- src/de/htwg/theAmazingLabyrinth/model/token.java
+++ src/de/htwg/theAmazingLabyrinth/model/token.java
@@ -2,11 +2,14 @@ package de.htwg.se.theAmazingLabyrinth.model;
 
 public class token {
 	String name = "";
+	int[] ways = new int[4];
 
 	public token() {
 		name = "standard token";
+		ways = {1,1,1,1};
 	}
-	public token(String s) {
+	public token(String s, int[4] ways) {
 		name = s;
+		this.ways = ways;
 	}
 }
