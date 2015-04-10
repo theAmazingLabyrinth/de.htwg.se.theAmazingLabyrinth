c2958f6f8037e4df8b95a322d2bfcc3d3e8f9eab
Benjamin Schaefer <beschaef@htwg-konstanz.de>
2015-04-10 12:35:59 +0200

    added 'end'symbol and modified constructors

--- src/de/htwg/theAmazingLabyrinth/model/token.java
+++ src/de/htwg/theAmazingLabyrinth/model/token.java
@@ -3,13 +3,16 @@ package de.htwg.se.theAmazingLabyrinth.model;
 public class token {
 	String name = "";
 	int[] ways = new int[4]; /* {North, East, South, West} */
+	int end;
 
 	public token() {
 		name = "standard token";
 		ways = {1,1,1,1};
+		end = 0;
 	}
-	public token(String s, int[4] ways) {
-		name = s;
+	public token(String name, int[4] ways, int end) {
+		this.name = name;
 		this.ways = ways;
+		this.end = end;
 	}
 }
