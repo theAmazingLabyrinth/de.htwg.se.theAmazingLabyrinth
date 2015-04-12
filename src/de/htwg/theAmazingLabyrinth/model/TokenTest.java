16d5be912bb2409cd0ee5e91be2600ca23fe7c00
Benjamin Schaefer <beschaef@htwg-konstanz.de>
2015-04-12 15:10:57 +0200

    modified TokenTest

--- src/de/htwg/theAmazingLabyrinth/model/TokenTest.java
+++ src/de/htwg/theAmazingLabyrinth/model/TokenTest.java
@@ -8,17 +8,29 @@ import org.junit.Test;
 public class TokenTest {
 
 	token Token;
+	token StandardToken;
 
 	@Before
 	public void setUp() throws Exception {
 		Token = new token("TestToken", new int[]{1,0,1,0}, 0);
+		StandardToken = new token();
 	}
 
 	@Test
 	public void testGetWay() {
-	assertEquals(0,Token.getWay(0));
-	assertEquals(0,Token.getWay(1));
+		for(int i = 0; i < 3; i ++) {
+			assertEquals(0, Token.getWay(0));
+			assertEquals(0, Token.getWay(1));
+			assertEquals(0, Token.getWay(-1));
+			assertEquals(0, Token.getWay(5));
+		}
 	} 
+	
+	@Test
+	public void testGetName() {
+		assertEquals("standard token", StandardToken.getName());
+		assertEquals("another token", StandardToken.getName());
+	}
 
 
 }
