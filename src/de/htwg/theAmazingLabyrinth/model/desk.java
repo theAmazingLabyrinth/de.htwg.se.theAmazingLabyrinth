8eee9a65a1875ba44dcfebf8771ae988d81f09a3
Daniel <dh.312@t-online.de>
2015-06-12 13:04:20 +0200

    add 3 Methodes (createMoveableTokens, createFirstHalf, createSecondHalf) + a Method getInt

--- src/de/htwg/theAmazingLabyrinth/model/desk.java
+++ src/de/htwg/theAmazingLabyrinth/model/desk.java
@@ -12,13 +12,73 @@ public class desk {
     public desk(){
         createFixTokens();
         createMoveableTokens();
-        for(int i=5; i<24; i++){
-            new mark[i-5] = i;
+        while(int i = 1; i <= 24; i++){
+            mark[i] = i;
         }
     }
 
+    //Prototyp!
+    /*create all moveable Tokens on the Table by Initialising*/
     private boolean createMoveableTokens(){
-        return true;
+        marknr = 0;
+        final int MAXITOKEN = 12;       //max. I - Tokens
+        final int MAXLTOKEN = 14;       //max. L - Tokens
+        final int MAXTTOKEN = 6;        //max. T - Token
+        int randomInt;                  //random (zahl auf englisch) to create random tokens
+        String names[]= new String[32]; //Array for the 32 moveable Tokens
+        boolean b_all;                  //counter if all tokens are created
+        int itoken; ltoken, ttoken;     //tokencounter
+
+        for(int i = 0;b_all != true;i++){
+            a = (int) (Math.random() * 2);
+            if (a = 0 && a <= maxItoken){names[i] = "I-Token";itoken++;}
+            if (a = 1 && a <= maxLtoken){names[i] = "L-Token";ltoken++;}
+            if (a = 2 && a <= maxTtoken){names[i] = "T-Token";ttoken++;}
+            if(itoken == MAXITOKEN; ltoken == MAXLTOKEN; ttoken == MAXTTOKEN){all = true;}
+        }
+
+        boolean b_first = createFirstHalf(s);
+        boolean b_second = createSecondHalf(s);
+        if(b_first == true && b_second == true){return true;}
+    }
+
+    /* create the first 12 moveable tokens */
+    private boolean createFirstHalf(String s){
+        for(int i = 0; i <= 6; i+2){
+            for(int j = 1; j <= 3; j++) {
+                if(getInt(s) == 2){
+                    standardDesk[i][(j*2)-1]=new token(s, getInt(s), mark[marknr]);
+                    marknr++;
+                } else {standardDesk[i][(j*2)-1] = new token(s, getInt(s), 0);
+            }
+            if(i == 6){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /*create the second 20 moevable tokens*/
+    private boolean createSecondHalf(String s){
+        for(int i = 1; i <= 5; i++){
+            for(int j = 0; j <= 6; j++){
+                if(getInt(s) == 2){
+                    standardDesk[i][(j*2)-1]=new token(s, getInt(s), mark[marknr]);
+                    marknr++;
+                } else {standardDesk[i][(j*2)-1] = new token(s, getInt(s), 0);
+            }
+            if(i == 5){
+                return true;
+            }
+            return false;
+        }
+    }
+
+    /* get as Int if the token is a "I-", "L-" or a "T- Token"*/
+    private int getInt(s){
+        if(s == "I-Token"){return 0;}
+        else if(s == "L-Token"){return 1;}
+        else {return 2;}
     }
 
     /* creates all fix Tokens on the Table by Initialising */
