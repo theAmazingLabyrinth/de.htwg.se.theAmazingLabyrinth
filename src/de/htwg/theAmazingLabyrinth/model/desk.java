730be97930447359bbf7d4b145f531a33c3e72cf
Daniel <dh.312@t-online.de>
2015-06-12 12:52:57 +0200

    add a mark-array, for the special tokens

--- src/de/htwg/theAmazingLabyrinth/model/desk.java
+++ src/de/htwg/theAmazingLabyrinth/model/desk.java
@@ -4,26 +4,29 @@ import java.lang.System;
 
 public class desk {
 
-    token[][] standardDesk = new token[7][7]; /*7*7 standard deskSize*/
+    private token[][] standardDesk = new token[7][7]; /*7*7 standard deskSize*/
+    private mark[] = new mark[24]; /* the array to count the special tokens, 1-4 = startplaces*/
+    private marknr; /*reader of the mark - array*/
 
     /*Initialising*/
     public desk(){
         createFixTokens();
         createMoveableTokens();
+        for(int i=5; i<24; i++){
+            new mark[i-5] = i;
+        }
     }
 
-    //Prototyp!
-    /*create all moveable Tokens on the Table by Initialising*/
-    public boolean createMoveableTokens(){
+    private boolean createMoveableTokens(){
         return true;
     }
 
     /* creates all fix Tokens on the Table by Initialising */
-    public boolean createFixTokens(){
-        standardDesk[0][0] = new token("L-Token",new int[]{0,1,1,0},0);
+    private boolean createFixTokens(){
+        standardDesk[0][0] = new token("L-Token",new int[]{0,1,1,0},1);
         standardDesk[0][2] = new token("T-Token",new int[]{0,1,1,1},0);
         standardDesk[0][4] = new token("T-Token",new int[]{0,1,1,1},0);
-        standardDesk[0][6] = new token("L-Token",new int[]{0,0,1,1},0);
+        standardDesk[0][6] = new token("L-Token",new int[]{0,0,1,1},2);
         standardDesk[2][0] = new token("T-Token",new int[]{1,1,1,0},0);
         standardDesk[2][2] = new token("T-Token",new int[]{0,1,1,1},0);
         standardDesk[2][4] = new token("T-Token",new int[]{1,0,1,1},0);
@@ -32,10 +35,10 @@ public class desk {
         standardDesk[4][2] = new token("T-Token",new int[]{1,1,1,0},0);
         standardDesk[4][4] = new token("T-Token",new int[]{1,1,0,1},0);
         standardDesk[4][6] = new token("T-Token",new int[]{1,0,1,1},0);
-        standardDesk[6][0] = new token("L-Token",new int[]{1,1,0,0},0);
+        standardDesk[6][0] = new token("L-Token",new int[]{1,1,0,0},3);
         standardDesk[6][2] = new token("T-Token",new int[]{1,1,0,1},0);
         standardDesk[6][4] = new token("T-Token",new int[]{1,1,0,1},0);
-        standardDesk[6][6] = new token("L-Token",new int[]{1,0,0,1},0);
+        standardDesk[6][6] = new token("L-Token",new int[]{1,0,0,1},4);
         return true;
     }
 }
