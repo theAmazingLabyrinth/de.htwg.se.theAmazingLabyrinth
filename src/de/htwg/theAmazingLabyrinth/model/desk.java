a265b92eae349f1413871c78e715263ea606c17a
Daniel <dh.312@t-online.de>
2015-06-17 14:52:31 +0200

    update createMoveableTokens to a less complex method

--- src/de/htwg/theAmazingLabyrinth/model/desk.java
+++ src/de/htwg/theAmazingLabyrinth/model/desk.java
@@ -1,15 +1,19 @@
 package de.htwg.theAmazingLabyrinth.model;
 
 import java.lang.System;
+import java.util.HashMap;
 
 public class desk {
 
-    private token[][] standardDesk = new token[7][7]; /*7*7 standard deskSize*/
-    private mark[] = new mark[24]; /* the array to count the special tokens, 1-4 = startplaces*/
-    private marknr; /*reader of the mark - array*/
+    private token[] standardDesk;
+    private int desklength;
+    private int mark[] = new mark[24]; /* the array to count the special tokens, 1-4 = startplaces*/
+    private int marknr = 0; /*reader of the mark - array*/
 
     /*Initialising*/
-    public desk(){
+    public desk(int deskLength){
+        this.deskLength = deskLength * deskLength
+        standardDesk = new token[this.deskLength];
         createFixTokens();
         createMoveableTokens();
         while(int i = 1; i <= 24; i++){
@@ -20,60 +24,44 @@ public class desk {
     //Prototyp!
     /*create all moveable Tokens on the Table by Initialising*/
     private boolean createMoveableTokens(){
-        marknr = 0;
         final int MAXITOKEN = 12;       //max. I - Tokens
         final int MAXLTOKEN = 14;       //max. L - Tokens
         final int MAXTTOKEN = 6;        //max. T - Token
-        int randomInt;                  //random (zahl auf englisch) to create random tokens
+        int randomInt;                  //random numbers to create random tokens
         String names[]= new String[32]; //Array for the 32 moveable Tokens
         boolean b_all;                  //counter if all tokens are created
         int itoken; ltoken, ttoken;     //tokencounter
 
-        for(int i = 0;b_all != true;i++){
+        for(int i = 0;b_all != false;i++){
             a = (int) (Math.random() * 2);
             if (a = 0 && a <= maxItoken){names[i] = "I-Token";itoken++;}
             if (a = 1 && a <= maxLtoken){names[i] = "L-Token";ltoken++;}
             if (a = 2 && a <= maxTtoken){names[i] = "T-Token";ttoken++;}
-            if(itoken == MAXITOKEN && ltoken == MAXLTOKEN && ttoken == MAXTTOKEN){all = true;}
+            if(itoken == MAXITOKEN && ltoken == MAXLTOKEN && ttoken == MAXTTOKEN){b_all = true;}
         }
 
-        boolean b_first = createFirstHalf(s);
-        boolean b_second = createSecondHalf(s);
-        if(b_first == true && b_second == true){return true;}
+        return boolean createMTokens(names);
     }
+}
 
     /* create the first 12 moveable tokens */
-    private boolean createFirstHalf(String s){
-        for(int i = 0; i <= 6; i+2){
-            for(int j = 1; j <= 3; j++) {
-                if(getInt(s) == 2){
-                    standardDesk[i][(j*2)-1] = new token(s, getInt(s), mark[marknr]);
-                    marknr++;
-                } else {standardDesk[i][(j*2)-1] = new token(s, getInt(s), 0);
-            }
-            if(i == 6){
-                return true;
-            }
-        }
-        return false;
-    }
+    private boolean createMTokens(String[] names) {
 
-    /*create the second 20 moevable tokens*/
-    private boolean createSecondHalf(String s){
-        for(int i = 1; i <= 5; i++){
-            for(int j = 0; j <= 6; j++){
-                if(getInt(s) == 2){
-                    standardDesk[i][(j*2)-1] = new token(s, getInt(s), mark[marknr]);
-                    marknr++;
-                } else {standardDesk[i][(j*2)-1] = new token(s, getInt(s), 0);
+        int namecounter = 0;
+        int i = 0;
+        int roundcounter = 1;
+        while (i <= desklength) {
+            if (rountcounter % 7 == 0) {
+                if (rount == true) {rount = false;}
+                else {rount = true;}
             }
-            if(i == 5){
-                return true;
-            }
-            return false;
+            standardDesk[i] = new token(names[namecounter], getInt(names[namecounter]), getMarkNr(names[namecounter]));
+            namecounter++;
+            rountcounter++;
+            if (rount == true) {i + 2;}
+            else {i++;}
         }
     }
-
     /* get as Int if the token is a "I-", "L-" or a "T- Token"*/
     private int getInt(s){
         if(s == "I-Token"){return 0;}
@@ -81,28 +69,39 @@ public class desk {
         else {return 2;}
     }
 
+    /* get the mark of the token */
+    getMarkNr(s) {
+        if (s == "T-Token"){
+            nr = mark[marknr];
+            marknr++;
+            return nr;
+        }
+        else{return 0;}
+    }
+
+
     /* creates all fix Tokens on the Table by Initialising */
     private boolean createFixTokens(){
-        standardDesk[0][0] = new token("L-Token",new int[]{0,1,1,0},1);
-        standardDesk[0][2] = new token("T-Token",new int[]{0,1,1,1},0);
-        standardDesk[0][4] = new token("T-Token",new int[]{0,1,1,1},0);
-        standardDesk[0][6] = new token("L-Token",new int[]{0,0,1,1},2);
-        standardDesk[2][0] = new token("T-Token",new int[]{1,1,1,0},0);
-        standardDesk[2][2] = new token("T-Token",new int[]{0,1,1,1},0);
-        standardDesk[2][4] = new token("T-Token",new int[]{1,0,1,1},0);
-        standardDesk[2][6] = new token("T-Token",new int[]{1,0,1,1},0);
-        standardDesk[4][0] = new token("T-Token",new int[]{1,1,1,0},0);
-        standardDesk[4][2] = new token("T-Token",new int[]{1,1,1,0},0);
-        standardDesk[4][4] = new token("T-Token",new int[]{1,1,0,1},0);
-        standardDesk[4][6] = new token("T-Token",new int[]{1,0,1,1},0);
-        standardDesk[6][0] = new token("L-Token",new int[]{1,1,0,0},3);
-        standardDesk[6][2] = new token("T-Token",new int[]{1,1,0,1},0);
-        standardDesk[6][4] = new token("T-Token",new int[]{1,1,0,1},0);
-        standardDesk[6][6] = new token("L-Token",new int[]{1,0,0,1},4);
+        standardDesk[0] = new token("L-Token",new int[]{0,1,1,0},1);
+        standardDesk[2] = new token("T-Token",new int[]{0,1,1,1},5);
+        standardDesk[4] = new token("T-Token",new int[]{0,1,1,1},6);
+        standardDesk[6] = new token("L-Token",new int[]{0,0,1,1},2);
+        standardDesk[14] = new token("T-Token",new int[]{1,1,1,0},7);
+        standardDesk[16] = new token("T-Token",new int[]{0,1,1,1},8);
+        standardDesk[18] = new token("T-Token",new int[]{1,0,1,1},9);
+        standardDesk[20] = new token("T-Token",new int[]{1,0,1,1},10);
+        standardDesk[28] = new token("T-Token",new int[]{1,1,1,0},11);
+        standardDesk[30] = new token("T-Token",new int[]{1,1,1,0},12);
+        standardDesk[32] = new token("T-Token",new int[]{1,1,0,1},13);
+        standardDesk[34] = new token("T-Token",new int[]{1,0,1,1},14);
+        standardDesk[42] = new token("L-Token",new int[]{1,1,0,0},3);
+        standardDesk[44] = new token("T-Token",new int[]{1,1,0,1},15);
+        standardDesk[46] = new token("T-Token",new int[]{1,1,0,1},16);
+        standardDesk[48] = new token("L-Token",new int[]{1,0,0,1},4);
         return true;
     }
 
-    public token[][] getstandardDesk(){
+    public token[]getstandardDesk(){
         return standardDesk;
     }
 }
