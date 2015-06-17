8679649725c7304fa80efc9b53b80e9e806872b7
Daniel <dh.312@t-online.de>
2015-06-17 18:40:39 +0200

    add constructor  + createTokenMap Method

--- src/main/java/de/htwg/theAmazingLabyrinth/controller/controller.java
+++ src/main/java/de/htwg/theAmazingLabyrinth/controller/controller.java
@@ -1,6 +1,39 @@
 package de.htwg.theAmazingLabyrinth.controller;
 
 
+import de.htwg.theAmazingLabyrinth.model.desk;
+import de.htwg.theAmazingLabyrinth.model.token;
+import java.util.HashMap;
+import java.util.Map;
+
 public class controller {
+
+    private Map<Integer, token> token;
+    private Map<Integer, String> player;
+    private token freeToken;
+    private int deskLength;
+    private int anzPlayer;
+    private desk Desk;
+
+    public controller(int anzPlayer, int tokenNumbers){
+        this.anzPlayer = anzPlayer;
+        token = new HashMap<Integer, token>();
+        player = new HashMap<Integer, String>();
+        Desk = new desk(tokenNumbers);
+        createTokenMap();
+        createPlayerMap(this.anzPlayer);
+    }
+
+    private void createTokenMap(){
+        token[] desk = Desk.getStandardDesk();
+
+        for(int i = 0; i <= desk.length; i++) {
+            token.put(i, desk[i]);
+        }
+    }
+
+    private void createPlayerMap(int anzPlayer){
+        return;
+    }
 
 }
