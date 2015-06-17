4a24fadf9ec67052ec0cf0b81637b5bb8b3ed64a
Daniel <dh.312@t-online.de>
2015-06-17 18:24:21 +0200

    add test for the Init of the controller class

--- src/test/java/de/htwg/theAmazingLabyrinth/model/ControllerTest.java
+++ src/test/java/de/htwg/theAmazingLabyrinth/model/ControllerTest.java
@@ -1,5 +1,16 @@
 package de.htwg.theAmazingLabyrinth.model;
 
+import de.htwg.theAmazingLabyrinth.controller.controller;
+import org.junit.Before;
+import org.junit.Test;
 
 public class ControllerTest {
+
+    controller Controller;
+
+    @Before
+    public void setUp() throws Exception {
+        Controller = new controller(4, 7);
+    }
+
 }
