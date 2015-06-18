4ef3a60221a0b6037468b37e10cc24654ecb3f4e
Daniel <dh.312@t-online.de>
2015-06-18 20:19:14 +0200

    create "createPlayerMap" Method

--- src/main/java/de/htwg/theAmazingLabyrinth/controller/controller.java
+++ src/main/java/de/htwg/theAmazingLabyrinth/controller/controller.java
@@ -12,16 +12,19 @@ public class controller {
     private Map<Integer, String> player;
     private token freeToken;
     private int deskLength;
+    private int tokenNumbers;
     private int anzPlayer;
+    private boolean b_player;
     private desk Desk;
 
     public controller(int anzPlayer, int tokenNumbers){
         this.anzPlayer = anzPlayer;
+        this.tokenNumbers = tokenNumbers;
         token = new HashMap<Integer, token>();
         player = new HashMap<Integer, String>();
-        Desk = new desk(tokenNumbers);
+        Desk = new desk(this.tokenNumbers);
         createTokenMap();
-        createPlayerMap(this.anzPlayer);
+        b_player = createPlayerMap(this.anzPlayer);
     }
 
     private void createTokenMap(){
@@ -32,8 +35,19 @@ public class controller {
         }
     }
 
-    private void createPlayerMap(int anzPlayer){
-        return;
+    private boolean createPlayerMap(int anzPlayer){
+        if(anzPlayer < 2){
+            return false;
+        }
+        if(anzPlayer == 3){
+            player.put(((tokenNumbers-1) * tokenNumbers)-1, "Player 3");
+        } else if(anzPlayer == 4) {
+            player.put(((tokenNumbers-1) * tokenNumbers)-1, "Player 3");
+            player.put((tokenNumbers * tokenNumbers)-1, "Player 4");
+        }
+        player.put(0, "Player 1");
+        player.put(tokenNumbers -1, "Player 2");
+        return true;
     }
 
 }
